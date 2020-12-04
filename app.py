e027988457ae73b23f7f26c54dae8af4c4d7b80d
Yash Kataria <yashkataria14.yk@gmail.com>
2020-12-04 13:38:31 +0530

    Initial Commit
    - Added requirements
    - Code for uploading images.
    - Added logger
    - Added necessary HTML pages

---
+++ app.py
@@ -0,0 +1,71 @@
+
+from flask import Flask, render_template, url_for, redirect, send_from_directory, request, flash
+import os, imghdr, logging
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+HOME_FOLDER = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = '{}/uploads/'.format(HOME_FOLDER)
+ALLOWED_EXTENSIONS = set(['jpg','png','jpeg','gif'])
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler('server.log')
+handler.setLevel(level=logging.INFO)
+
+log_formatter = logging.Formatter("%(asctime)s - %(message)s")
+handler.setFormatter(log_formatter)
+
+logger.addHandler(handler)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'h233'
+
+def check_file_extension(filename):
+    logging.info('Checking file extension')
+    if '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+        return True
+    return False
+
+def create_new_folder(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    return  dir_path 
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/upload/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        logging.info(app.config['UPLOAD_FOLDER'])
+        if 'image' not in request.files:
+            flash('No file')
+            return redirect(url_for('home'))
+        _file = request.files['image']
+        if _file.filename == '':
+            flash('No selected file')
+            return redirect(url_for('home'))
+        if _file and check_file_extension(_file.filename):
+            logging.info('Saving file')
+            filename = secure_filename(_file.filename)
+            create_new_folder(app.config['UPLOAD_FOLDER'])
+            saved_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            _file.save(saved_path)
+            logging.info('File saved')
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+        else:
+            flash('Only images are allowed')
+        return redirect(url_for('home'))        
+    return redirect(url_for('home'))
+
+
